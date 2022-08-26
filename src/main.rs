bca4b9755edfce93b7d89003004139df8a70aa0c
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-26 20:48:41 +0900

    feat(main): read arguments keyword, path

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::{env, process};
+
 fn main() {
-    // implement your logic
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Missing arguments");
+        process::exit(1);
+    }
+    if args.len() > 3 {
+        eprintln!("Too much arguments");
+        process::exit(1);
+    }
+    let keyword = args[1].clone();
+    let path = args[2].clone();
 }
