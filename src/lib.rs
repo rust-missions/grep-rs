37a793612f4b26d5cf1abb8783596d5f2143951b
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-26 21:26:22 +0900

    refactor(lib): use iterator instead of getting specific index

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::process;
+use std::fmt;
 
 pub struct Args {
     pub keyword: String,
@@ -7,17 +7,18 @@ pub struct Args {
 
 impl Args {
     pub fn build(args: &[String]) -> Result<Args, &'static str> {
-        if args.len() > 3 {
-            return Err("Too many arguments");
-        }
-        let keyword = match args.get(1) {
+        let mut args = args.iter().skip(1);
+        let keyword = match args.next() {
             Some(keyword) => keyword.clone(),
             None => return Err("missing arguments: keyword, path"),
         };
-        let path = match args.get(2) {
+        let path = match args.next() {
             Some(path) => path.clone(),
             None => return Err("missing arguments: path"),
         };
+        if args.len() > 0 {
+            return Err("Too many arguments");
+        }
 
         Ok(Args { keyword, path })
     }
