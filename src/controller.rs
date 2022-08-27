b82e6447b62530bcb31e7f75be1698625d0a2bb3
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-28 00:05:50 +0900

    refactor(main): cleanse error handling with controller

---
+++ src/controller.rs
@@ -0,0 +1,16 @@
+use crate::error::Error;
+use crate::{search, Target, ThreadPool};
+
+pub fn run(args: Vec<String>) -> Result<(), Error> {
+    let target: Target = Target::build(&args)?;
+    let workload = &target.paths.len();
+    if workload > &1 {
+        ThreadPool::new(workload)?.run(target)?;
+        return Ok(());
+    }
+    match target.paths.get(0) {
+        Some(path) => println!("{}", search::run(&target.keyword,path)),
+        None => return Err(Error::FileSystemError),
+    };
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use crate::pool::ThreadPool;
 use crate::target::Target;
 use std::{env, process};
 
+mod controller;
 mod error;
 mod pool;
 mod search;
@@ -9,27 +10,8 @@ mod target;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let target: Target = match Target::build(&args) {
-        Ok(target) => target,
-        Err(e) => {
-            eprintln!("{}", e);
-            process::exit(1);
-        }
+    if let Err(e) = controller::run(args) {
+        eprintln!("{}", e);
+        process::exit(1);
     };
-    let workload = &target.paths.len();
-    if workload > &1 {
-        let thread_pool = match ThreadPool::new(workload) {
-            Ok(pool) => pool,
-            Err(e) => {
-                eprintln!("{}", e);
-                process::exit(1);
-            }
-        };
-        if let Err(e) = thread_pool.run(target) {
-            eprintln!("{}", e);
-            process::exit(1);
-        };
-        return
-    }
-    println!("{}", search::run(&target.keyword, &target.paths.get(0).unwrap()));
 }
